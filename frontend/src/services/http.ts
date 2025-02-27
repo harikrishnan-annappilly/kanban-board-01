1ea50398de917c6aa2278d5196ed526d54e3e919
Hari <harikrishnan.annappilly@gmail.com>
2025-02-27 09:26:32 +0530

    [frontend]: Hostname changed for axios

--- frontend/src/services/http.ts
+++ frontend/src/services/http.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const baseURL = "http://localhost:5000/api";
+const baseURL = `http://${location.hostname}:5000/api`;
 
 const http = axios.create({
-  baseURL,
+    baseURL,
 });
 
 export default http;
